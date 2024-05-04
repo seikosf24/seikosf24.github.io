8ea7765fc3726efd94cee082b392204434b1523c
Glitch (glitch-blank-website) <none>
2024-05-04 08:23:46 +0000

    👱🎮 Checkpoint ./script.js:34453384/256 ./style.css:34453384/7721 ./.glitch-assets:34453384/778 ./index.html:34453384/31

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.getElementById("openbtn").addEventListener("click", function () {
     document.getElementById("openbtn").classList.toggle("active");
     document.getElementById("g-nav").classList.toggle("panelactive");
   });
+
+let topImage = 0;
+setInterval(function() {
+  topImage ++;
+  console.log(topImage);
+}, 2000);
