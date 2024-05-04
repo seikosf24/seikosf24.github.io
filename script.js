c64d376dc3355b2add867b05a1794b09a01665d1
Glitch (glitch-blank-website) <none>
2024-05-04 09:13:10 +0000

    🦀⛪️ Checkpoint ./script.js:34453384/2632 ./style.css:34453384/389 ./index.html:34453384/610

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.getElementById("openbtn").addEventListener("click", function () {
   document.getElementById("g-nav").classList.toggle("panelactive");
 });
 
+//手作りスライドショー(ガチで時間かかった)
 let arrey = [
   "https://cdn.glitch.global/6b229fd4-b092-4044-9cc2-0b306f76df3b/IMG_6340.jpeg?v=1714780045834",
   "https://cdn.glitch.global/6b229fd4-b092-4044-9cc2-0b306f76df3b/IMG_6346.jpeg?v=1714810752604",
@@ -14,10 +15,13 @@ let element = document.getElementById("topImage");
 let style = window.getComputedStyle(element);
 setInterval(function () {
   topImage++;
-  if (topImage % 2 == 0){
-  element.style.opacity = 0.5 - (style.getPropertyValue("opacity") - 0.5);
+  if (topImage % 2 == 0) {
+    element.style.opacity = 0.5 - (style.getPropertyValue("opacity") - 0.5);
   }
-  document.getElementById("topImage").style.backgroundImage = arrey[(Math.floor(topImage / 4) + 1 ) % 5]
-  
-  console.log(arrey[topImage]);
+  document.getElementById("topImage").style.backgroundImage = `url("${
+    arrey[(Math.floor(topImage / 4) * 2 + 1) % arrey.length]
+  }"`;
+  document.getElementById("topImage2").style.backgroundImage = `url("${
+    arrey[(Math.floor((topImage + 2) / 4) * 2) % arrey.length]
+  }"`;
 }, 1500);
