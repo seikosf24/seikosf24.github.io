84bf0ade47ae13cd185f0d73d6ce0b405e5f023c
Glitch (glitch-blank-website) <none>
2024-05-04 04:49:00 +0000

    🌕🌱 Checkpoint ./style.css:34453384/107 ./index.html:34453384/5471 ./script.js:34453384/217

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("openbtn").addEventListener("click", function () {
+    document.getElementById("openbtn").classList.toggle("active");
+    document.getElementById("g-nav").classList.toggle("panelactive");
+  });
