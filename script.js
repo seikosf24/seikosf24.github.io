8bd53f2756b1d435469e6643cb990e5a63adb53f
Glitch (glitch-blank-website) <none>
2024-05-13 13:28:43 +0000

    🍥🌼 Checkpoint ./script.js:34453384/39

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ document.getElementById("pagetop").addEventListener("click", function () {
 //フッター
 let footer = document.getElementsByTagName("footer")[0];
 footer.innerHTML = `<div id="footerLink">
+        <a href="/">HOME</a>
         <a href="#">INFO</a>
         <a href="#">BOOTH</a>
         <a href="#">STAGE</a>
