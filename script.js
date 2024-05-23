b349f999d9a0254afc2390975d71ce522e736cb7
Glitch (glitch-blank-website) <none>
2024-05-23 07:41:48 +0000

    🏩👧 Checkpoint ./style.css:34453384/471 ./index.html:34453384/15 ./script.js:34453384/1211

--- script.js
+++ script.js
@@ -31,6 +31,48 @@ document.getElementById("pagetop").addEventListener("click", function () {
     behavior: "smooth",
   });
 });
+//カウントダウン
+let CD = document.getElementById("counterDown");
+let theDay = new Date(2024, 11, 3, 9, 0, 0);
+    // 現在の「曜日 月 日 年 時 分 秒」を取得
+    let nowDate = new Date();
+    // 現在が西暦何年か取得 (This Year)
+    let tYear = nowDate.getFullYear();
+    // 現在が何月か取得 (This Month)
+    let tMonth = nowDate.getMonth();
+    // 現在が何日か取得 (This Date)
+    let tDate = nowDate.getDate();
+
+    // nowDateを「1970年1月1日00時00分00秒」から何ミリ秒経過したかに変換
+    let now = new Date(tYear, tMonth, tDate).getTime();
+    
+    console.log(nowDate);
+    console.log(now);
+    console.log(tYear);
+    // 指定月日の年を指定
+    let nYear = tYear;
+    // 指定月日の月を指定 -1を忘れない！！！
+    let nMonth = 1 - 1;
+    // 指定月日の日を指定
+    let nDate = 22;
+    
+    // 指定月日を「曜日 月 日 年 時 分 秒」に変換
+    let n = new Date( nYear, nMonth, nDate );
+    // nを「1970年1月1日00時00分00秒」から何ミリ秒経過したかに変換
+    let nTarget = n.getTime();
+
+    console.log(nYear);
+    console.log(n);
+    console.log(nTarget);
+    // 現在から指定月日まであと何ミリ秒か計算
+    let diffDate = nTarget - now;
+
+    // 上で求めたミリ秒を日に変換
+    // 指定日までの日数 = 指定月日までのミリ秒 / ( 1000 * 60秒 * 60分 * 24時間 )
+    let date = Math.floor( diffDate / ( 1000 * 60 * 60 * 24 ));
+
+    console.log(diffDate);
+    console.log(date);
 
 //フッター
 let footer = document.getElementsByTagName("footer")[0];
