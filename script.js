2f0ffe2251db9748591b60e7dd50bca215c4c2b0
Glitch (glitch-blank-website) <none>
2024-05-23 22:32:51 +0000

    👂🐢 Checkpoint ./index.html:34453384/45878 ./style.css:34453384/2931 ./script.js:34453384/602

--- script.js
+++ script.js
@@ -6,32 +6,6 @@ document.getElementById("openbtn").addEventListener("click", function () {
   document.getElementById("g-nav").classList.toggle("panelactive");
 });
 
-//トップ遷移のやつ表示
-let firster = document.body.firstChild;
-let new_element = document.createElement("div");
-new_element.id = "pagetop";
-firster.before(new_element);
-//トップ遷移の表示非表示
-window.addEventListener("scroll", function () {
-  appear();
-});
-//高さで表示非表示
-function appear() {
-  if (window.scrollY > 700) {
-    document.getElementById("pagetop").style.right = "20px";
-  } else {
-    document.getElementById("pagetop").style.right = "-60px";
-  }
-}
-appear();
-//ページトップへ遷移
-document.getElementById("pagetop").addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
-
 
 //フッター
 let footer = document.getElementsByTagName("footer")[0];
