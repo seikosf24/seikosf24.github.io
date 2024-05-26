4dbf23df087207394b8ef82fbc4547790db775f7
Glitch (sf24) <none>
2024-05-26 09:43:16 +0000

    🌅🙇 Checkpoint ./info/index.html:34453384/8683 ./booth/index.html:34453384/6120 ./style.css:34453384/95 ./script.js:34453384/7

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let footer = document.getElementsByTagName("footer")[0];
 footer.innerHTML = `<div id="footerLink">
         <a href="/">HOME</a>
         <a href="/info">INFO</a>
-        <a href="#">BOOTH</a>
+        <a href="/booth">BOOTH</a>
         <a href="#">STAGE</a>
         <a href="#">NEWS</a>
         <a href="#">FAQ</a>
