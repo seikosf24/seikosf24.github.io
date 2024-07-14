59c6f55774673e0d06af3c14d3b5f92be4c103bf
Glitch (sf24) <none>
2024-07-14 08:37:52 +0000

    🗺🎮 Checkpoint ./stage/index.html:34453384/49 ./faq/index.html:34453384/1696 ./script.js:34453384/6030 ./booth/index.html:34453384/1696

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ document.getElementById("openbtn").addEventListener("click", function () {
   document.getElementById("g-nav").classList.toggle("panelactive");
 });
 
-
 //フッター
 let footer = document.getElementsByTagName("footer")[0];
 footer.innerHTML = `<div id="footerLink">
@@ -19,3 +18,65 @@ footer.innerHTML = `<div id="footerLink">
       </div>
       &copy; School Fair Steering Committee
       <div id="footerCoolLogo">SEIKO SF '24</div>`;
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".details").forEach(function (el) {
+    const summary = el.querySelector(".summary");
+    const answer = el.querySelector(".answer");
+    summary.addEventListener("click", (event) => {
+      // デフォルトの挙動を無効化
+      event.preventDefault();
+      // detailsのopen属性を判定
+      if (el.getAttribute("open") !== null) {
+        // アコーディオンを閉じるときの処理
+        const closingAnim = answer.animate(
+          closingAnimation(answer),
+          animTiming
+        );
+
+        closingAnim.onfinish = () => {
+          // アニメーションの完了後にopen属性を取り除く
+          el.removeAttribute("open");
+        };
+      } else {
+        // open属性を付与
+        el.setAttribute("open", "true");
+        // アコーディオンを開くときの処理
+        const openingAnim = answer.animate(
+          openingAnimation(answer),
+          animTiming
+        );
+      }
+    });
+  });
+});
+
+// アニメーションの時間とイージング
+const animTiming = {
+  duration: 300,
+  easing: "ease-in-out",
+};
+// アコーディオンを閉じるときのキーフレーム
+const closingAnimation = (answer) => [
+  {
+    height: answer.offsetHeight + "px",
+    opacity: 1,
+  },
+  {
+    height: 0,
+    opacity: 0,
+  },
+];
+// アコーディオンを開くときのキーフレーム
+const openingAnimation = (answer) => [
+  {
+    height: 0,
+    opacity: 0,
+  },
+  {
+    height: answer.offsetHeight + "px",
+    opacity: 1,
+  },
+];
