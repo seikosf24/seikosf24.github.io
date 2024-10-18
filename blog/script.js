17a1a1ed17583d782dfb3e69e7973afeb4a85448
Yuya <yuya3195@iCloud.com>
2024-10-18 21:19:51 +0900

    大々的に実験

--- blog/script.js
+++ blog/script.js
@@ -1,5 +1,7 @@
 let aside = document.getElementById("aside");
 aside.innerHTML = "横に書く文章。おそらくリンクリストとかタグとか、そう言うのを並べる。";
 
-let blogUrl = location.pathname.replace(/\/+$/, "").split('/').pop();
-console.log(blogUrl.slice(0, 1));
+const blogUrl = location.pathname.replace(/\/+$/, "").split('/').pop();
+let before = ("0" + (blogUrl - 1)).slice(0, 2);
+let after = ("0" + (blogUrl + 1)).slice(0, 2);
+alert(before + "+" + after);
