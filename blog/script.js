5a26966e7abe471417db753c46b47d17d34ca2ed
Yuya <yuya3195@iCloud.com>
2024-10-18 21:01:50 +0900

    細かい部分

--- blog/script.js
+++ blog/script.js
@@ -1,2 +1,5 @@
 let aside = document.getElementById("aside");
 aside.innerHTML = "横に書く文章。おそらくリンクリストとかタグとか、そう言うのを並べる。";
+
+let blogUrl = location.pathname.replace(/\/+$/, "").split('/').pop();
+console.log(blogUrl.slice(0,1));
