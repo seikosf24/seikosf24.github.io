f116513436d89afd3e19bfccbb7c1ecbd942169d
Yuya <yuya3195@iCloud.com>
2024-10-19 07:19:25 +0900

    Update script.js

--- blog/script.js
+++ blog/script.js
@@ -5,12 +5,13 @@ const blogUrl = Number(location.pathname.replace(/\/+$/, "").split('/').pop().sl
 let before = ("00" + (blogUrl - 1)).slice(-2);
 let after = ("00" + (blogUrl + 1)).slice(-2);
 document.getElementById("channel").innerHTML =
-    `<a href='./${before}.html'>前のブログ</a>
+    `<a href='./${after}.html'>次のブログ</a>
     <a href = '../'> ブログトップ</a>
-    <a href='./${after}.html'>次のブログ</a>`;
+    <a href='./${before}.html'>前のブログ</a>`;
 
 let data = document.getElementById("letter");
 let dataInside = data.innerHTML;
+dataInside = dataInside.replaceAll("{{武", `<div class="selif"><div class="selifIcon iinncho">武`);
 dataInside = dataInside.replaceAll("{{", `<div class="selif"><div class="selifIcon">`);
 dataInside = dataInside.replaceAll("}}", `</div></div>`);
 dataInside = dataInside.replaceAll("}{", `</div><div class="selifText">`);
