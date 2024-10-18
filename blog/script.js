cd22689aa9d5aac24174593afa085aa6f8f2c1fc
Yuya <yuya3195@iCloud.com>
2024-10-18 21:07:55 +0900

    前日やけど実験

--- blog/script.js
+++ blog/script.js
@@ -2,4 +2,4 @@ let aside = document.getElementById("aside");
 aside.innerHTML = "横に書く文章。おそらくリンクリストとかタグとか、そう言うのを並べる。";
 
 let blogUrl = location.pathname.replace(/\/+$/, "").split('/').pop();
-console.log(blogUrl.slice(0,1));
+console.log(blogUrl.slice(0, 1));
