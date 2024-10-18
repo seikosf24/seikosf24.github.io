cf1dc5f39e77c07d3a6735a26b4c4e8b68423c77
Yuya <yuya3195@iCloud.com>
2024-10-18 21:32:18 +0900

    結果にcommit

--- blog/script.js
+++ blog/script.js
@@ -2,6 +2,6 @@ let aside = document.getElementById("aside");
 aside.innerHTML = "横に書く文章。おそらくリンクリストとかタグとか、そう言うのを並べる。";
 
 const blogUrl = location.pathname.replace(/\/+$/, "").split('/').pop();
-let before = ("00" + (blogUrl + (-1))).slice(-2);
-let after = ("00" + (blogUrl + 1)).slice(-2);
-alert(before + "+" + after);
+let before = ("00" + (Number(blogUrl) - 1)).slice(-2);
+let after = ("00" + (Number(blogUrl) + 1)).slice(-2);
+alert(blogUr + "+" + before + "+" + after);
