58b293a7633a81ceba77e9f8b0c8fa0b36085060
Yuya <yuya3195@iCloud.com>
2024-10-18 22:18:19 +0900

    e

--- blog/script.js
+++ blog/script.js
@@ -1,5 +1,5 @@
 let aside = document.getElementById("aside");
-aside.innerHTML = "横に書く文章。おそらくリンクリストとかタグとか、そう言うのを並べる。";
+aside.innerHTML = `Archive`;
 
 const blogUrl = Number(location.pathname.replace(/\/+$/, "").split('/').pop().slice(0, 2));
 let before = ("00" + (blogUrl - 1)).slice(-2);
@@ -8,3 +8,10 @@ document.getElementById("channel").innerHTML =
     `<a href='./${before}.html'>前のブログ</a>
     <a href = '../'> ブログトップ</a>
     <a href='./${after}.html'>次のブログ</a>`;
+
+let data = document.getElementById("letter");
+let dataInside = data.innerHTML;
+dataInside = dataInside.replaceAll("{{", `<div class="selif"><div class="selifIcon">`);
+dataInside = dataInside.replaceAll("}}", `</div></div>`);
+dataInside = dataInside.replaceAll("}{", `</div><div class="selifText">`);
+data.innerHTML = dataInside;
