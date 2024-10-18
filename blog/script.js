f8a4b3229b82ce434aef6397aaa2e8e7ccdb876f
Yuya <yuya3195@iCloud.com>
2024-10-18 21:47:35 +0900

    u

--- blog/script.js
+++ blog/script.js
@@ -1,7 +1,9 @@
 let aside = document.getElementById("aside");
 aside.innerHTML = "横に書く文章。おそらくリンクリストとかタグとか、そう言うのを並べる。";
 
-const blogUrl = location.pathname.replace(/\/+$/, "").split('/').pop();
-let before = ("00" + (Number(blogUrl.slice(0, 2)) - 1)).slice(-2);
-let after = ("00" + (Number(blogUrl.slice(0, 2)) + 1)).slice(-2);
-alert(blogUrl + "+" + before + "+" + after);
+const blogUrl = Number(location.pathname.replace(/\/+$/, "").split('/').pop().slice(0, 2));
+let before = ("00" + (blogUrl - 1)).slice(-2);
+let after = ("00" + (blogUrl + 1)).slice(-2);
+document.getElementById("channel").innerHTML = `<a href='./${before}.html'>前のブログ</a>
+    <a href = '../'> ブログトップ</a>
+        <a href='./${after}.html'>次のブログ</a>`;
