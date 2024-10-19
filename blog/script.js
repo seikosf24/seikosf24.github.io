48e44c664405d392c087b200c30bd10f2fa4ad80
Yuya <yuya3195@iCloud.com>
2024-10-20 07:15:01 +0900

    一番初めのページは戻れない

--- blog/script.js
+++ blog/script.js
@@ -1,14 +1,23 @@
 let aside = document.getElementById("aside");
-aside.innerHTML = `Archive`;
+aside.innerHTML = `<h3>Article</h3>
+<a href="./01.html>Webサイト開設！</a><br>
+
+`;
 
 const blogUrl = Number(location.pathname.replace(/\/+$/, "").split('/').pop().slice(0, 2));
 let before = ("00" + (blogUrl - 1)).slice(-2);
 let after = ("00" + (blogUrl + 1)).slice(-2);
+if (before != "00") {
 document.getElementById("channel").innerHTML =
     `<a href='./${after}.html'>次のブログ</a>
     <a href = '../'> ブログトップ</a>
     <a href='./${before}.html'>前のブログ</a>`;
-
+} else {
+document.getElementById("channel").innerHTML =
+    `<a href='./${after}.html'>次のブログ</a>
+    <a href = '../'> ブログトップ</a>
+    <a tabindex="-1"><u>前のブログ</u></a>`;
+}
 let data = document.getElementById("letter");
 let dataInside = data.innerHTML;
 dataInside = dataInside.replaceAll("{{武", `<div class="selif"><div class="selifIcon iinncho">武`);
